dbdd10d743b53cc9bfcbc18f02fd506b32952d44
Deepakbaria <deepak.bariya@techiegent.com>
2023-03-14 14:55:32 +0530

    feat/hariom

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Hello World header 1</h1>
+    </div>
+  )
+}
+
+export default Header
+
+
+
+
+export const Header2 = () =>{
+    return(
+        <div>
+            <h1>hello World header 2</h1>
+        </div>
+    )
+}
